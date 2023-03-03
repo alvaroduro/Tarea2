ce997674a655126a57362a331d32687952fc7224
duro86 <alvaroda86@gmail.com>
2023-03-03 21:06:54 +0100

    Teste1

--- test/com/AlvaroDuran/Ejercicio5Test.java
+++ test/com/AlvaroDuran/Ejercicio5Test.java
@@ -26,6 +26,7 @@ public class Ejercicio5Test {
         Ejercicio5.main(args);
         // TODO review the generated test code and remove the default call to fail.
         fail("The test case is a prototype.");
+        System.out.println("jdbwebdiwebd");
     }
     
 }
